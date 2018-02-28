8cff211f03935df481173e005485b1a9c5d0810c
Dirk Seidensticker <dirk.seidensticker@gmail.com>
2018-02-28 08:15:47 +0100

    removed early test Fig3.R

--- FIG3.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-
-d <- read.csv("https://raw.githubusercontent.com/dirkseidensticker/aDRAC/master/data/aDRAC.csv")
-
-View(d)
-
-ggplot(d, aes(x=LONG, y=LAT, color = MATERIAL)) + 
-  geom_point()
-
